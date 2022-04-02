5aa51f2a07548c24dc1c73488ae12942af3415e3
Oscar Caballero <oscarcaballero.dev@gmail.com>
2022-04-01 20:28:29 -0500

    change beforeUnmount for onUnmounted

--- src/js/components/Splide/Splide.vue
+++ src/js/components/Splide/Splide.vue
@@ -31,20 +31,28 @@
 </template>
 
 <script lang="ts">
-import { ComponentConstructor, Options, Splide } from '@splidejs/splide';
-import { computed, defineComponent, onBeforeUnmount, onMounted, onUpdated, PropType, ref, watch } from 'vue';
-import { EVENTS } from '../../constants/events';
-import { isEqualShallow, merge } from '../../utils';
-
+import { ComponentConstructor, Options, Splide } from "@splidejs/splide";
+import {
+  computed,
+  defineComponent,
+  onUnmounted,
+  onMounted,
+  onUpdated,
+  PropType,
+  ref,
+  watch,
+} from "vue";
+import { EVENTS } from "../../constants/events";
+import { isEqualShallow, merge } from "../../utils";
 
 /**
  * The component for the Splide root element.
  *
  * @since 0.4.0
  */
-export default defineComponent( {
-  name: 'Splide',
-  emits: EVENTS.map( event => `splide:${ event }` ),
+export default defineComponent({
+  name: "Splide",
+  emits: EVENTS.map((event) => `splide:${event}`),
 
   props: {
     /**
@@ -68,52 +76,56 @@ export default defineComponent( {
     hasSliderWrapper: Boolean,
   },
 
-  setup( props, context ) {
+  setup(props, context) {
     const { options } = props;
     const splide = ref<Splide>();
-    const root   = ref<HTMLElement>();
+    const root = ref<HTMLElement>();
     let slides: HTMLElement[] = [];
 
-    onMounted( () => {
-      if ( root.value ) {
-        splide.value = new Splide( root.value, props.options );
-        bind( splide.value );
-        splide.value.mount( props.extensions, props.transition );
+    onMounted(() => {
+      if (root.value) {
+        splide.value = new Splide(root.value, props.options);
+        bind(splide.value);
+        splide.value.mount(props.extensions, props.transition);
       }
-    } );
+    });
 
-    onBeforeUnmount( () => {
+    onUnmounted(() => {
       splide.value?.destroy();
-    } );
+    });
 
-    onUpdated( () => {
-      if ( splide.value ) {
+    onUpdated(() => {
+      if (splide.value) {
         const newSlides = getSlides();
 
-        if ( ! isEqualShallow( slides, newSlides ) ) {
+        if (!isEqualShallow(slides, newSlides)) {
           splide.value.refresh();
           slides = newSlides;
         }
       }
-    } );
+    });
 
-    if ( options ) {
-      watch( () => merge( {}, options ), options => {
-        if ( splide.value ) {
-          splide.value.options = options;
-        }
-      }, { deep: true } );
+    if (options) {
+      watch(
+        () => merge({}, options),
+        (options) => {
+          if (splide.value) {
+            splide.value.options = options;
+          }
+        },
+        { deep: true }
+      );
     }
 
     /**
      * Returns the current index.
      */
-    const index = computed( () => splide.value?.index || 0 );
+    const index = computed(() => splide.value?.index || 0);
 
     /**
      * Returns the latest number of slides.
      */
-    const length = computed( () => splide.value?.length || 0 );
+    const length = computed(() => splide.value?.length || 0);
 
     /**
      * Goes to the slide specified by the control.
@@ -122,8 +134,8 @@ export default defineComponent( {
      *
      * @param control - A control pattern.
      */
-    function go( control: number | string ): void {
-      splide.value?.go( control );
+    function go(control: number | string): void {
+      splide.value?.go(control);
     }
 
     /**
@@ -131,8 +143,8 @@ export default defineComponent( {
      *
      * @param target - A target to sync.
      */
-    function sync( target: Splide ): void {
-      splide.value?.sync( target );
+    function sync(target: Splide): void {
+      splide.value?.sync(target);
     }
 
     /**
@@ -142,12 +154,12 @@ export default defineComponent( {
      *
      * @param splide - A splide instance.
      */
-    function bind( splide: Splide ): void {
-      EVENTS.forEach( event => {
-        splide.on( event, ( ...args: any[] ) => {
-          context.emit( `splide:${ event }`, splide, ...args );
-        } );
-      } );
+    function bind(splide: Splide): void {
+      EVENTS.forEach((event) => {
+        splide.on(event, (...args: any[]) => {
+          context.emit(`splide:${event}`, splide, ...args);
+        });
+      });
     }
 
     /**
@@ -158,9 +170,9 @@ export default defineComponent( {
      * @return An array with slide elements.
      */
     function getSlides(): HTMLElement[] {
-      if ( splide.value ) {
+      if (splide.value) {
         const children = splide.value.Components.Elements?.list.children;
-        return children && Array.prototype.slice.call( children ) || [];
+        return (children && Array.prototype.slice.call(children)) || [];
       }
 
       return [];
@@ -173,7 +185,7 @@ export default defineComponent( {
       length,
       go,
       sync,
-    }
+    };
   },
-} );
+});
 </script>
